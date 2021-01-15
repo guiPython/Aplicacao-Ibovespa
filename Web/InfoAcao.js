14f5d22e0f620e4f8d3b078901054138ee058744
Guilherme Rocha <67191432+guiPython@users.noreply.github.com>
2021-01-15 17:19:53 -0300

    Update InfoAcao
    Suporte para BDRs

--- Web/InfoAcao.js
+++ Web/InfoAcao.js
@@ -4,18 +4,34 @@ const axios = require('axios')
 
 class InfoAcao {
 
-    constructor( nomeAcao , _keyAlphaVantage ,_linkStatusInvest,_linkYahooFinance,_linkAlphaVantage,_linkAlphaVantageTimeSeries ){
+    constructor( nomeAcao , _keyAlphaVantage ,_linkStatusInvestAcoes,_linkStatusInvestBDRs,_linkYahooFinance,_linkAlphaVantage,_linkAlphaVantageTimeSeries ){
         this.nomeAcao = nomeAcao
         this._linkAlphaVantage = "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=@none.SAO&apikey=@keyAlpha"
         this._linkYahooFinance = "https://query1.finance.yahoo.com/v8/finance/chart/none.SA?region=US&lang=en-US&includePrePost=false&interval=2m&range=1d&corsDomain=finance.yahoo.com&.tsrc=finance"
-        this._linkStatusInvest = "https://statusinvest.com.br/acoes/@none"
+        this._linkStatusInvestAcoes = "https://statusinvest.com.br/acoes/@none"
+        this._linkStatusInvestBDRs = "https://statusinvest.com.br/bdrs/@none"
         this._linkAlphaVantageTimeSeries = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=@none.SAO&outputsize=full&apikey=@keyAlpha"
         this._keyAlphaVantage = _keyAlphaVantage
     };
 
+    async verifyTick(){
+        let cTick = this.nomeAcao.length
+        if ( cTick == 5 ) return "Acoes"
+        if ( cTick == 6 ) {
+            let numTick = this.nomeAcao.slice(-2)
+            if( ["34","35"].includes(numTick) ) return "BDRs"
+            else if (["11","12"].includes(numTick)) return "Acoes"
+            else { throw new Error("Insira um codigo valido") }
+        }
+        
+    }
+
     async getStaticInfo(){
         try {
-            return axios.get(this._linkStatusInvest.replace('@none',this.nomeAcao)).then( response => {
+            let tick = await this.verifyTick()
+            if ( tick == "BDRs"){ var url = this._linkStatusInvestBDRs.replace('@none',this.nomeAcao) }
+            if ( tick == "Acoes"){ var url = this._linkStatusInvestAcoes.replace("@none",this.nomeAcao)}
+            return axios.get(url).then( response => {
             let $ = cheerio.load(response.data)
             let empresa = $("h1.lh-4").attr("title");
             let setor = $("div.pr-md-2").find("strong.value").first().text();
@@ -23,7 +39,7 @@ class InfoAcao {
             return [String(empresa), String(setor) , String(subsetor)]})
         }
         catch {
-            return 'Not Found'
+            throw new Error("Not Found")
         }
     }
 
@@ -34,7 +50,7 @@ class InfoAcao {
             })
         }
         catch{
-            return 'Not Found'
+            throw new Error("Not Found")
         }
     }
 
@@ -51,7 +67,7 @@ class InfoAcao {
             })
         }
         catch {
-            return 'Not Found'
+            throw new Error("Not Found")
         }
 
     }
@@ -63,7 +79,7 @@ class InfoAcao {
             })
         }
         catch {
-            return 'Not Found'
+            throw new Error("Not Found")
         }
     }
 
@@ -72,6 +88,7 @@ class InfoAcao {
 async function teste_GetStaticInfo( nomeAcao ) {  
     const acao = new InfoAcao(nomeAcao) ; 
     const info = await acao.getStaticInfo() ; 
+    console.log()
     console.log(`Empresa: ${info[0]}\nSetor: ${info[1]}\nSubSetor: ${info[2]}`) 
 }
 
@@ -96,7 +113,7 @@ async function teste_GetTimeSerieJSON( nomeAcao ){
     const info = await acao.getTimeSeriesJSON();
     console.log(info)
 }
+["prio3","petr4","aapl34","meli34"].forEach(x => {teste_GetStaticInfo(x)})
 
-//teste_GetTimeSerieJSON('prio3')
 
 module.exports = InfoAcao
